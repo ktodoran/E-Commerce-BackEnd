38e08eaf88fb312fdd53e2ad66354e9c44108c19
ktodoran <ktodoran@gmail.com>
2021-08-11 21:34:29 -0400

    Finished all the model js files

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {foreignKey: 'categoric_id'});
 
 // Categories have many Products
+Category.hasMany(Product, {foreignKey: 'categoric_id'});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'assoc_products',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
